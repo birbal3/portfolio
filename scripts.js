51591d4690645777bbbba9cd9eeb913d12397627
Birbal <119029678+birbal3@users.noreply.github.com>
2024-08-27 02:47:09 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,12 @@
+
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
